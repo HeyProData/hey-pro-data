4a1078641c3687535a4e2166db039cc1a21808df
Anant Kumar <anantkumar012002@gmail.com>
2025-11-28 15:02:54 +0530

    style: implement InboxPage and AppLayout components for chat functionality

---
+++ app/(app)/(chat)/inbox/page.tsx
@@ -0,0 +1,7 @@
+export default function InboxPage() {
+    return (
+        <main className="p-10">
+            <h1 className="text-2xl font-bold text-black">Inbox Page</h1>
+        </main>
+    )
+}
